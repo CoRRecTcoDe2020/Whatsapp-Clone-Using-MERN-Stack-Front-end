64b035748ab4cef7926ef078f4ee213e3ee68e02
CoRRecT CoDe <77756002+CoRRecTcoDe2020@users.noreply.github.com>
2021-10-24 01:53:09 +0330

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000", // Enter Your URL
+  baseURL: "http://localhost:9000", // Enter Your backend server url
 });
 
 export default instance
